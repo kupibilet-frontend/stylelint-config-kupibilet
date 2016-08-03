0396dc87faacd14d656988f8374815a73f537da7
Valentin Semirulnik <v7rulnik@gmail.com>
2016-08-03 19:18:26 +0300

    Don't install stylelint-config-standard

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 
 (
   export PKG=stylelint-config-standard;
-  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev "$PKG"
+  npm info "$PKG" peerDependencies --json | command sed 's/[\{\},]//g ; s/: /@/g' | xargs npm install --save-dev
 )
